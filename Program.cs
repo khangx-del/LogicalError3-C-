76bb80be1fa6b5a0d922e6ebf0ce061549fd7e21
NguyenKhang <NguyenKhang@EDU-DTWKY74>
2025-11-12 13:46:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalError3_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]  {1, 2, 3, 4, 5 };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];            
+            }
+            
+            Console.WriteLine("Sum of numbers: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
